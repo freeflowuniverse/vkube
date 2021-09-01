0d7ae2b23a5c9908d9bf1d5535f6be58261f7e9d
Maxime Daniel <root@maxux.net>
2021-09-01 23:48:48 +0200

    network: support real call to kubernetes api

--- vkubeng/vkubeng.v
+++ vkubeng/vkubeng.v
@@ -1,4 +1,6 @@
 module crystalkube
+
+import net.http
 import encoding.base64
 
 struct Kube {
@@ -7,8 +9,11 @@ mut:
 
 	host string
 	port int
+	endpoint string
 
-	rootca string  // certificate-authority-data
+	url string
+
+	verify string  // certificate-authority-data
 	cert string    // client-certificate-data
 	key string     // client-key-data
 }
@@ -17,27 +22,55 @@ mut:
 
 
 // object manager
-pub fn kube_get() Kube {
-	return Kube{debug: true}
+pub fn kube_get(host string, port int, endpoint string) Kube {
+	url := "https://$host:$port$endpoint"
+
+	return Kube{
+		debug: true,
+		host: host,
+		port: port,
+		endpoint: endpoint,
+		url: url
+	}
 }
 
 // private functions
-fn (k Kube) request(url string) {
+fn (k Kube) get(url string) ? string {
 	println("fetching $url")
+
+	vurl := "$k.url$url"
+
+	r := http.fetch(
+		validate: true
+		url: vurl,
+		verify: k.verify,
+		cert: k.cert,
+		cert_key: k.key
+
+	) ?
+
+	return r.text
+
 }
 
 // public functions
 
 // rootca, cert and key should be base64 encoded
 // this is /etc/rancher/k3s/k3s.yaml in k3os
-pub fn (mut k Kube) authorize(rootca string, cert string, key string) {
-	k.rootca = base64.decode(rootca).bytestr()
+pub fn (mut k Kube) authorize(rootca string, cert string, key string) bool {
+	/*
+	k.verify = base64.decode(rootca).bytestr()
 	k.cert = base64.decode(cert).bytestr()
 	k.key = base64.decode(key).bytestr()
+	*/
+
+	k.verify = rootca
+	k.cert = cert
+	k.key = key
 
 	if k.debug {
 		println("== Root CA Certificate ==")
-		println(k.rootca)
+		println(k.verify)
 
 		println("== User Certificate ==")
 		println(k.cert)
@@ -45,16 +78,27 @@ pub fn (mut k Kube) authorize(rootca string, cert string, key string) {
 		println("== User Private Key ==")
 		println(k.key)
 	}
+
+	check := k.get("/api") or {
+		println("nope")
+		return false
+	}
+
+	println(check)
+
+	return true
 }
 
 pub fn (k Kube) api_info() {
 	println("COUCOU")
-	k.request("/api")
+	// k.request("/api")?
 }
 
-pub fn (k Kube) pods_gets() {
+pub fn (k Kube) pods_gets() ? string {
 	println("fetching pods")
-	k.request("/api/v1/namespaces/default/pods")
+
+	r := k.get("/v1/namespaces/default/pods")?
+	return r
 }
 
 pub fn (k Kube) pod_get() {
